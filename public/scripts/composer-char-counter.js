b625667be5aff946e2042b3a9d432bdf2a00e7df
shinmike <shinmike90@gmail.com>
2017-06-13 20:43:07 +0000

    Task 6: Target & Update the counter element

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,7 +2,8 @@ $(document).ready(function(){
   $('.new-tweet textarea').on('input', function(){
     const charLimit = 140;
     const charLength = $(this).val().length;
-    const charRemaining = charLimit - charLength;
+    const charRemaining = charLimit - charLimit;
+    console.log(charRemaining);
     const $counter = $(this).parent().children('.counter');
     $counter.text(charRemaining);
   });
