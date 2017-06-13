6c4fc9edd9cbedf006ec54f3f93f230ab258bcc2
shinmike <shinmike90@gmail.com>
2017-06-13 20:41:24 +0000

    revise how charRemaining is calculated

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,7 +2,8 @@ $(document).ready(function(){
   $('.new-tweet textarea').on('input', function(){
     const charLimit = 140;
     const charLength = $(this).val().length;
-    const charRemaining = charLength - charLimit;
-    console.log(charRemaining);
+    const charRemaining = charLimit - charLength;
+    const $counter = $(this).parent().children('.counter');
+    $counter.text(charRemaining);
   });
 });
