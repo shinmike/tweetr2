33c1272ecdfca5ab44e64f84bc23b9e4bd237850
shinmike <shinmike90@gmail.com>
2017-06-13 20:12:53 +0000

    Task 3: Bind event handler, Task 4: this

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
   console.log("document ready!");
+  $('.new-tweet textarea').on('input', function(){
+    console.log($(this).val().length);
+  });
 });
 
 console.log("composer-char-counter added!");
