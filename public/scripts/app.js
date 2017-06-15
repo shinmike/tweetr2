a8ae61e8b016fc3b4e5ccd01aff8813facad7444
shinmike <shinmike90@gmail.com>
2017-06-15 00:18:55 +0000

    Form Validation

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,60 +1,29 @@
+function showError(message){
+  alert(message);
+};
+
 $(function(){
-  var data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": {
-          "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-          "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-        },
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": {
-          "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-          "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-        },
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    },
-    {
-      "user": {
-        "name": "Johann von Goethe",
-        "avatars": {
-          "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-          "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-        },
-        "handle": "@johann49"
-      },
-      "content": {
-        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-      },
-      "created_at": 1461113796368
-    }
-  ];
 
   $('#createTweet').on('submit', function(event) {
     event.preventDefault();
+    var tweetLength = $('#textTweet').val().length;
+
+    if (tweetLength === 0) {
+      showError("You didn't tweet anything :(");
+      return;
+    }; 
+
+    if (tweetLength > 140){
+      showError("You exceeded 140 characters :(");
+      return;
+    };
+
     $.ajax({
       method: "POST",
       url: "/tweets",
       data: $(this).serialize()
-    }).done(function(data){
+    }).done(function(){
       console.log('AJAX POST request successful!!!');
-      console.log('data!!!', data);
       loadTweets();
     })
   });
@@ -101,11 +70,8 @@ $(function(){
       data: $(this).serialize()
     }).done(function(data){
       console.log('AJAX GET request successful!!!');
-      console.log('data!!!', data);
       renderTweets(data);
     })
   }
-
-  // renderTweets(data);
 
 });
